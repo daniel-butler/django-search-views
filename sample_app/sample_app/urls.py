772f00754d2b1627c5f5a079e16ab65d81a44a13
Daniel Butler <me@daniel-butler.com>
2022-07-29 15:18:04 -0400

    update for django 3+

--- sample_app/sample_app/urls.py
+++ sample_app/sample_app/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 from django.contrib import admin
 from actors.views import ActorsView
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', ActorsView.as_view(), name="actors"),
+    path('admin/', admin.site.urls),
+    path('', ActorsView.as_view(), name="actors"),
 ]
